cf1fc3e74391a9be1bebfdf10c1f2e1be35cfe07
Pedro Victori <fosforilasa@gmail.com>
2019-09-11 00:36:25 +0100

    finishing TreePlanter class

--- src/main/java/behaviour_trees/core/TreePlanter.java
+++ src/main/java/behaviour_trees/core/TreePlanter.java
@@ -1,5 +1,261 @@
 package behaviour_trees.core;
 
-public class TreePlanter {
+import behaviour_trees.composite.*;
+import behaviour_trees.testing.Main;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.awt.*;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.List;
+
+public class TreePlanter {
+	private enum TagName {
+		ARGS("args"),
+		ARG("arg"),
+		TASK("task"),
+		BRANCHES("branches"),
+		GUARD("guard");
+		private String name;
+		private TagName(String name) {
+			this.name = name;
+		}
+
+		private String getName() {
+			return name;
+		}
+	}
+	private enum TaskType {
+		LEAF("leaf"),
+		COMPOSITE("composite");
+
+		private String type;
+		private TaskType(String type) {
+			this.type = type;
+		}
+
+		private String getType() {
+			return type;
+		}
+	}
+	private enum Attribute {
+		TYPE("type"),
+		CLASS("class");
+
+		String name;
+		Attribute(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
+	private enum CompositeClass {
+		SELECTOR("selector", Selector.class),
+		SEQUENCE("sequence", Sequence.class),
+		PARALLEL("parallel", ParallelTask.class),
+		RANDOM_SEQUENCE("rnd-sequence", RandomSequence.class),
+		RANDOM_SELECTOR("rnd-selector", RandomSelector.class);
+
+		private String name;
+		private Class<CompositeTask> classRef;
+		private CompositeClass(String name, Class classRef) {
+			this.name = name;
+			this.classRef = classRef;
+		}
+
+		private String getName() {
+			return name;
+		}
+
+		private Class<CompositeTask> getClassRef() {
+			return classRef;
+		}
+	}
+	private Document document;
+	private Schema schema;
+	private String fileName;
+	private Map<String, Class<? extends Task>> classLibrary;
+
+	public TreePlanter(String fileName, Map<String, Class<? extends Task>> classLibrary) {
+		this.fileName = fileName;
+		this.classLibrary = classLibrary;
+		try {
+			schema = loadSchema();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		Document document = getValidatedDocument();
+	}
+
+	public Tree plantTree(int id) {
+		//find trunk and tree guard if present
+		Element root = document.getDocumentElement();
+
+		Tree tree = new Tree(id, parseTask(root, id), parseArgs(root));
+		return tree;
+	}
+
+	private CompositeTask parseCompositeTask(Element element, int id) {
+		//int id, List<Task> branches, Guard guard
+		String classAttr = element.getAttribute(Attribute.CLASS.getName());
+		Class<CompositeTask> cTask = null;
+		for (CompositeClass value : CompositeClass.values()) {
+			if (classAttr.equals(value.getName())) {
+				cTask = value.getClassRef();
+			}
+		}
+		if (cTask == null) {
+			cTask = (Class<CompositeTask>) classLibrary.get(classAttr);
+		}
+
+		return createCompositeInstance(cTask, id, parseBranches(element, id), parseGuard(element, id), parseArgs(element));
+	}
+
+	private List<Task> parseBranches(Element root, int id) {
+		List<Task> branches = new ArrayList<>();
+		Element branchesRoot = (Element) root.getElementsByTagName(TagName.BRANCHES.getName()).item(0);
+		NodeList branchesNodes = branchesRoot.getChildNodes();
+		for (int i = 0; i < branchesNodes.getLength(); i++) {
+			Element branchElement = (Element) branchesNodes.item(i);
+			branches.add(parseTask(branchElement, id));
+		}
+		return branches;
+	}
+
+	private Task parseLeafTask(Element element, int id) {
+		return createInstanceFrom(element, id, parseGuard(element, id), parseArgs(element));
+	}
+
+	private Task parseTask(Element element, int id) {
+		String taskType = element.getAttribute(Attribute.TYPE.getName());
+		Task thisTask = null;
+		if (taskType.equals(TaskType.COMPOSITE.getType())) {
+			parseCompositeTask(element, id);
+		} else if (taskType.equals(TaskType.LEAF.getType())) {
+			thisTask = parseLeafTask(element, id);
+		} else {
+			throw new IllegalArgumentException("Invalid task type");
+		}
+		return thisTask;
+	}
+	private Guard parseGuard(Element element, int id) {
+		if (element.getElementsByTagName(TagName.GUARD.getName()).getLength() == 0) { //has no guard
+			return null;
+		} else {
+			Element guardElement = (Element) element.getElementsByTagName(TagName.GUARD.getName()).item(0);
+			Task guard = createInstanceFrom(guardElement, id, parseGuard(guardElement, id), parseArgs(guardElement)); //guard can itself be guarded
+			if(guard instanceof Guard) return (Guard) guard;
+			else throw new IllegalArgumentException("Invalid class definition");
+		}
+	}
+
+	private CompositeTask createCompositeInstance(Class<CompositeTask> cClass, int id, List<Task> branches, Guard guard, String... args) {
+		Constructor<CompositeTask> constructor = null;
+		try {
+			constructor = cClass.getConstructor(
+					int.class,
+					List.class,
+					Guard.class,
+					String[].class);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+
+		//create new instance and return it
+		try {
+			return constructor.newInstance(id, branches, guard, args);
+		} catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private Task createInstanceFrom(Element element, int id, Guard guard, String... args) {
+		String className = element.getAttribute(Attribute.CLASS.getName());
+		Class<? extends Task> loadedClass = classLibrary.get(className);
+		Constructor<? extends Task> constructor = null;
+
+		try {
+			constructor = loadedClass.getConstructor(
+					int.class,
+					Guard.class,
+					String[].class);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+
+		//create new instance and return it
+		try {
+			return constructor.newInstance(id, guard, args);
+		} catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String[] parseArgs(Element element) {
+		if (element.getElementsByTagName(TagName.ARGS.getName()).getLength() == 0) { //has no args
+			return new String[0];
+		} else {
+			Node argsNode = element.getElementsByTagName(TagName.ARGS.getName()).item(0);
+			NodeList argNodes = argsNode.getChildNodes();
+			String[] args = new String[argNodes.getLength()];
+			for (int i = 0; i < argNodes.getLength(); i++) {
+				args[i] = argNodes.item(i).getNodeValue();
+			}
+			return args;
+		}
+	}
+
+	private Document getValidatedDocument() {
+		DocumentBuilderFactory builderFactory =
+				DocumentBuilderFactory.newInstance();
+		builderFactory.setSchema(schema);
+		builderFactory.setIgnoringElementContentWhitespace(true);
+		DocumentBuilder builder = null;
+
+		try {
+			builder = builderFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		Document document = null;
+		try {
+			document = builder.parse((Main.class.getClassLoader().getResourceAsStream("mp.xml")));
+		} catch (SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
+		Validator validator = schema.newValidator();
+
+		try {
+			validator.validate(new DOMSource(document));
+		} catch (SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		return document;
+	}
+
+	private Schema loadSchema() throws SAXException {
+		String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+		SchemaFactory factory = SchemaFactory.newInstance(language);
+		schema = factory.newSchema((Main.class.getClassLoader().getResource("bt.xsd")));
+		return schema;
+	}
 }
